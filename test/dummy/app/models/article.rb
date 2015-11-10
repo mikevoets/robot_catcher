7e812f6ec43c4d3e9d152e48d6ac64cafd9afc02
Mike Wilhelmus Hendrikus Gerar Voets <mvo010@ifilab81.stud.cs.uit.no>
2015-11-10 10:39:03 +0100

    gitignore is now working

--- test/dummy/app/models/article.rb
+++
@@ -1,3 +0,0 @@
-class Article < ActiveRecord::Base
-  rc_validate :title, :text
-end
--- test/robot_catcher_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RobotCatcherTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, RobotCatcher
-  end
-end
